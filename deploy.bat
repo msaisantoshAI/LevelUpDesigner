ad2e4f539a9d00edfbaf579b97dd9c6ebc20a568
Sai Santosh <saisantosh@levelupdesigners.com>
2026-08-13 19:26:10 +0530

    Initial commit: LevelUP Designers - Interactive Product Design Deck

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 echo =======================================================
 echo LevelUP Designers - GitHub Deployment
 echo =======================================================
-echo Target: https://github.com/msaisantoshAI/LevelUp-Designer.git
+echo Target: https://github.com/msaisantoshAI/LevelUpDesigner.git
 echo.
 set /p GITHUB_TOKEN="Enter your GitHub Personal Access Token (or press Enter if configured): "
 node push_git.js %GITHUB_TOKEN%
